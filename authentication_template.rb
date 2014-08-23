8a57bc177797e452f2b8119a42cab908fc0c12ca
Serdar Dogruyol <dogruyolserdar@gmail.com>
2014-08-23 18:03:34 +0300

    Added Authentication Template

---
+++ authentication_template.rb
@@ -0,0 +1,38 @@
+gem_group :development, :test do
+  gem 'rspec-rails', '~> 3.0.0'
+  gem 'factory_girl_rails'
+end
+
+gem_group :development do
+  gem 'capistrano', '2.15.2'
+  gem 'rvm-capistrano'
+  gem 'quiet_assets'
+  gem 'better_errors'
+  gem 'annotate', '>=2.6.0'
+  gem 'bullet'
+  gem 'faker'
+  gem 'lol_dba'
+end
+
+gem_group :test do
+  gem 'minitest'
+  gem 'guard-rspec', require: false
+  gem 'capybara'
+  gem 'shoulda'
+end
+
+# Use Devise For Authentication
+gem 'devise'
+
+# You should really install libxml and other required libraries system-wide
+run 'bundle config build.nokogiri --use-system-libraries'
+if yes?('Do you want to install your bundle to vendor/bundle? (yes/no)')
+  run 'bundle install --path vendor/bundle'
+else
+  run 'bundle install'
+end
+run 'bundle exec rails g rspec:install'
+run 'guard init'
+run 'bundle exec rails g devise:install'
+run 'bundle exec rails g devise User'
+run 'bundle exec rake db:migrate'
